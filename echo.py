c8ca0f6c07cb507a75add2b0b0bd58280dba0dc0
Brad Smith <brad.alan.smith@gmail.com>
2016-06-20 18:53:37 -0700

    remove logg

--- echo.py
+++ echo.py
@@ -1,14 +1,12 @@
 
 import json
 
-import flask
-Flask, request = flask.Flask, flask.request
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def echo():
-    print request.args
     return json.dumps(request.args)
 
 if __name__ == '__main__':
