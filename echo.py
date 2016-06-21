af72e7f1aca01d9754a4990bd2ddcf35e5b64c6a
Brad Smith <brad.alan.smith@gmail.com>
2016-06-21 09:49:05 -0700

    jsonfiy

--- echo.py
+++ echo.py
@@ -1,13 +1,13 @@
 
 import json
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/echo')
 def echo():
-    return json.dumps(request.args)
+    return jsonify(request.args)
 
 if __name__ == '__main__':
     app.run(debug=True)
